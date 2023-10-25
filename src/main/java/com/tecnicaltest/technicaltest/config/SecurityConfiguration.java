ebee4dbf2933764c74e660c0e3e39780b8713611
Elias Garcia Carvajal <ega.carvajal@gmail.com>
2023-10-25 13:07:08 -0300

    Se agrega metodo SecurityFilterChain securityFilterChain con alternativa AntPathRequestMatcher en vez de MvcRequestMatcher

--- src/main/java/com/tecnicaltest/technicaltest/config/SecurityConfiguration.java
+++ src/main/java/com/tecnicaltest/technicaltest/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.tecnicaltest.technicaltest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -22,27 +24,39 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-  private final AuthenticationProvider authenticationProvider;
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  @Autowired
+  private AuthenticationProvider authenticationProvider;
+  @Autowired
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
   private static final String API_URL_PATTERN = "/auth/**";
 
-
-  public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
-    this.authenticationProvider = authenticationProvider;
-    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-  }
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+//    MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+//    http.csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN), PathRequest.toH2Console()));
+//    http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+//    http.authorizeHttpRequests(auth -> auth.requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll().requestMatchers(PathRequest.toH2Console()).authenticated().anyRequest().authenticated()).authenticationProvider(authenticationProvider).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+//    http.formLogin(Customizer.withDefaults());
+//    http.httpBasic(Customizer.withDefaults());
+//    return http.build();
+//  }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-    MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
-    http.csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN), PathRequest.toH2Console()));
-    http.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-    http.authorizeHttpRequests(auth -> auth.requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll()
-            .requestMatchers(PathRequest.toH2Console()).authenticated().anyRequest().authenticated())
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(
+            new AntPathRequestMatcher(API_URL_PATTERN), new AntPathRequestMatcher("/h2-console/**")))
+        .headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(new AntPathRequestMatcher(API_URL_PATTERN)).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).authenticated()
+            .anyRequest().authenticated()
+        )
         .authenticationProvider(authenticationProvider)
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    http.formLogin(Customizer.withDefaults());
-    http.httpBasic(Customizer.withDefaults());
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .formLogin(Customizer.withDefaults())
+        .httpBasic(Customizer.withDefaults());
+
     return http.build();
   }
 
